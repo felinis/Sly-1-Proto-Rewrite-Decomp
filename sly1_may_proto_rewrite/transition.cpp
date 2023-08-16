633396ddc525164a1c2f74f9a552d380c7ab84bc
theclub654 <61169936+theclub654@users.noreply.github.com>
2023-08-16 15:33:30 -0400

    Update transition.cpp

--- sly1_may_proto_rewrite/transition.cpp
+++ sly1_may_proto_rewrite/transition.cpp
@@ -14,7 +14,7 @@ void CTransition::Set(char* pchzWorld, OID oidWarp, OID oidWarpContext, GRFTRANS
 void CTransition::Execute()
 {
 	SetPhase(PHASE_Load); // Setting the global game state to load because where loading a file
-	CBinaryInputStream file("jb_intro");
+	CBinaryInputStream file("jb_intro"); // THIS IS HERE TEMPORARILY I PLAN ON REWRITING HOW THE GAME LOADS FILES, INSTEAD OF A WAL IM GONNA STORE ALL ASSET FILES IN A FOLDER
 	DeleteSw(g_psw); // Deleting SW object
 	g_psw = nullptr;
 	SetupBulkDataFromBrx(1, &file);
